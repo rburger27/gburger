3244a08e44130561d641728bba7a7ae490e63433
Gary Burger <rburger27@gmail.com>
2017-09-19 01:19:45 -0500

    First Commit of DIR from Main PC
    First Push of data from main PC

---
+++ Academic Grading System/app.js
@@ -0,0 +1,37 @@
+function valueRange(){
+  var range = parseFloat(document.getElementById('gradeScore').value);
+  if (range > 100) {
+    alert("Number cannot be over 100")
+  }
+  else if (range < 0) {
+    return false;
+  }
+  else {
+    gradeCheck();
+  }
+  }
+
+function gradeCheck() {
+var ngrade = parseFloat(document.getElementById('gradeScore').value);
+if(ngrade > 100){
+         document.getElementById('letterGrade').value = "Please Try Again!";
+  }
+  else if(100 >= ngrade && ngrade >= 90){
+       document.getElementById('letterGrade').value = "A";
+  }
+  else if(89 >= ngrade && ngrade >= 80){
+         document.getElementById('letterGrade').value = "B";
+  }
+  else if(79 >= ngrade && ngrade >= 70){
+         document.getElementById('letterGrade').value = "C";
+  }
+  else if(69 >= ngrade && ngrade >= 60){
+         document.getElementById('letterGrade').value = "D";
+  }
+  else if(59 >= ngrade && ngrade >= 0){
+         document.getElementById('letterGrade').value = "F";
+  }
+  else {
+    document.getElementById('letterGrade').value = "Please Try Again!";
+  }
+}
