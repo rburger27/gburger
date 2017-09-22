e0f2576e68f8e56bbeb082df401449e3b710b911
Gary Burger <rburger27@gmail.com>
2017-09-22 00:59:20 -0500

    Final Submission of BING BONG Logged to Console

--- Bing Bong/app.js
+++ Bing Bong/app.js
@@ -1,13 +1,16 @@
-for (var i = 1; i <= 100; i++) {
-  if ((i % 3 == 0) && (i % 5 == 0)){
-    console.log("Bing Bong")
-  }
-  if (i % 3 == 0) {
-    console.log("Bing")
-  }
-  if (i % 5 == 0) {
-    console.log("Bong");
-  }else{
-    console.log(i);
+
+function run(){
+  for (var i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)){
+      console.log("Bing Bong");
+    }
+    else if (i % 3 == 0) {
+      console.log("Bing");
+    }
+    else if (i % 5 == 0) {
+      console.log("Bong");
+    }else{
+      console.log(i);
+    }
   }
 }
