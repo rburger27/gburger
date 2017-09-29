504994ae8609dad6d3dd1c9a297e89ed728ea5dc
Gary Burger <rburger27@gmail.com>
2017-09-29 00:49:55 -0500

    Output to Console and printed

--- Complex Functions Sequences/app.js
+++ Complex Functions Sequences/app.js
@@ -1,3 +1,18 @@
-function run() {
-  
+function run(){
+  n =  parseFloat(document.getElementById('amount').value) -1 ;
+  var numGen = function (n)
+{
+  if (n===1)
+  {
+    return [1, 1];
+  }
+  else
+  {
+    var s = numGen(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+  console.log(numGen(n))
+  document.getElementById('output').innerHTML = numGen(n) + "";
 }
