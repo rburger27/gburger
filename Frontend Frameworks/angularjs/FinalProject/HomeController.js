ba48889b8497a0e7ae32b99512e25f33ef699eaa
Gary Burger <rburger27@gmail.com>
2017-10-01 23:46:27 -0500

    Struction test

---
+++ Frontend Frameworks/angularjs/FinalProject/HomeController.js
@@ -0,0 +1,7 @@
+class HomeController {
+  constructor() {
+    this.myName = 'Gary';
+  }
+}
+// register angular with the main app module
+angular.module('finalproject');.controller('HomeController', HomeController);
